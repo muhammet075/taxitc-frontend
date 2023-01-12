328257582820e35fdef384e96c8fc5638bde23a4
Muhammet Komur <muhammet@dentsuachtung.com>
2023-01-12 16:51:53 +0100

    adding head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,20 @@
 import '../styles/globals.css';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return(
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+        <meta name="format-detection" content="telephone=no"/>
+        <meta name="description" content="Taxi T.C."/>
+        <meta property="og:title" content="Taxi T.C." />
+        <meta name="keywords" content="Taxi T.C."/>
+        <meta property="og:url" content="https://www.taxitc.nl/"/>
+        <meta property="og:image" content="/oglogo.png"/>
+      </Head>
       <Header/>
       <main>
         <Component {...pageProps} />
